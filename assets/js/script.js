84433f660ea332a03f12352f0433a849d1026aad
darakhshanda <darakhshanda@hotmail.com>
2025-11-10 17:52:27 +0000

    add: eventlistener to butttons in script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,53 @@
-!function() {
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+// Add event listeners to each button 
+  
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked Submit");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+            }
+        });
+    }
+function runGame() {
     console.log("Hello, World!");
-}();
+}
+function checkAnswer(){
+    return true;
+}
+function calculateCorrectAnswer() {
+    return 42;
+}
+function incrementScore() {
+    let currentScore = parseInt(document.getElementById("score").innerText);
+}
+function incrementWrongAnswer() {
+    let currentWrongScore = parseInt(document.getElementById("wrong-score").innerText);
+}
+function displayAdditionQuestion() {
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
+    document.getElementById("question").innerText = `${num1} + ${num2}`;
+}
+function displaySubtractionQuestion() {
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
+    document.getElementById("question").innerText = `${num1} - ${num2}`;
+}   
+function displayMultiplicationQuestion() {
+    let num1 = Math.floor(Math.random() * 12) + 1;
+    let num2 = Math.floor(Math.random() * 12) + 1;
+    document.getElementById("question").innerText = `${num1} * ${num2}`;
+
+}
+function displayDivisionQuestion() {
+    let num1 = Math.floor(Math.random() * 12) + 1;  
+    let num2 = Math.floor(Math.random() * 12) + 1;  
+    let dividend = num1 * num2; 
+    document.getElementById("question").innerText = `${dividend} / ${num1}`;
+}
+});
+   
