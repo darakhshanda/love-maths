3728576dfef6ae604f9a4b68e6b0c5d5d6b4c4f5
darakhshanda <darakhshanda@hotmail.com>
2025-11-11 00:17:51 +0000

    Complete Division challenge

--- assets/js/script.js
+++ assets/js/script.js
@@ -52,7 +52,8 @@ function runGame( gameType) {
 function checkAnswer(){
     let userAnswer = document.getElementById("answer-box").value;
     let calculatedAnswer = calculateCorrectAnswer();
-    let isCorrect = parseInt(userAnswer) === calculatedAnswer[0];
+    //typecast user answer and calculated answer to integers
+    let isCorrect = parseInt(userAnswer) === parseInt(calculatedAnswer[0]);
     // authenticate answer
     if (isCorrect) {
         alert("Hey! You got it right! :D");
@@ -81,13 +82,22 @@ function calculateCorrectAnswer() {
     } else if (operator === "x") {
         return [operand1 * operand2, "multiply"];
     } else if (operator === "/") {
-        return [operand1 / operand2, "division"];
+        // check for division by zero
+        if (operand1 === 0 || operand2 === 0) {
+            alert("Division by zero is not allowed.");
+            throw "Division by zero is not allowed. Aborting!";
+        } else if (operand1 % operand2 === 0) { // check if division has no remainder
+            return [(operand1 / operand2).toPrecision(3), "division"]; // return answer with 3 significant digits
+        } else {
+            return [operand1 / operand2, "division"]; // return exact integer answer in other cases
+        }
     } else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
     }
     
 }
+
 /**
  * Gets the current score from the DOM and increments it by 1   
  */
@@ -126,9 +136,11 @@ function displayMultiplicationQuestion(operand1, operand2) {
 
 }
 function displayDivisionQuestion(operand1, operand2) {
-
-    document.getElementById("operand1").innerText = operand1;
-    document.getElementById("operand2").innerText = operand2;
+// check if operand1 is less than operand2
+  document.getElementById("operand1").innerText = operand1 > operand2 ? operand1 : operand2;
+    // set operand2 to the smaller number
+    document.getElementById("operand2").innerText = operand1 > operand2 ? operand2 : operand1;
+    
     document.getElementById("operator").innerText = "/";
 }
 });
